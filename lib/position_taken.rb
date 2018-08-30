53a708a10bfa4071c8b43e3b6a609bf3748aa7e4
Allison Tearjen <amtearjen@ucdavis.edu>
2018-08-30 21:23:17 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board,number)
   index = number -1
-  if board[index] == " X" || board[index] == " O" || board[index] == "O " || board[index] == " O "
+  if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == " " || board[index] == ""
+  elsif board[index] == " "
     return false
   elsif board[index] == nil
     return false
