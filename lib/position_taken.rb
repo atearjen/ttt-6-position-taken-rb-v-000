291c6bdecb1caaecdb9a66adb71ec75a660aca35
Allison Tearjen <amtearjen@ucdavis.edu>
2018-08-30 21:20:26 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board,number)
   index = number -1
   if board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == "  " || board[index] == " "
+  elsif board[index] == " " || board[index] == ""
     return false
   elsif board[index] == nil
     return false
